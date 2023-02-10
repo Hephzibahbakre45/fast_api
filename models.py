51566b180f6140926ee02ca355ae60203f553f5f
Hephzibahbakre45 <122830190+Hephzibahbakre45@users.noreply.github.com>
2023-02-10 14:30:48 +0100

    Update models.py

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class User(Base):
     username=Column(String(25), unique=True)
     email=Column(String(80), unique=True)
     password=Column(Text, nullable=True)
-    is_staff=Column(Boolean, default=False)
+    is_staff=Column(Boolean, default=True)
     is_active=Column(Boolean, default=False)
     orders = relationship("Order",back_populates='user')
 
